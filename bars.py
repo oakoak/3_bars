30182111ea81c78025433eb175a441c3b4d491bd
oakoak <oakduboak.gmail.com>
2017-10-07 16:37:08 +0300

    Добавленно описание Э

--- bars.py
+++ bars.py
@@ -1,29 +1,27 @@
 import json
 
 
-def get_seat_count(data):
-	return data['properties']['Attributes']['SeatsCount']
-
-
 def load_data(filepath):
 	f = open(filepath, 'r')
 	return json.loads(f.read())
 
 
 		
-def get_biggest_bar(data):
-	return max(data['features'], key=get_seat_count)
+def get_biggest_bar(bars_json):
+	get_seat_count = lambda bar: bar['properties']['Attributes']['SeatsCount']
+	return max(bars_json['features'], key=get_seat_count)
 
 
-def get_smallest_bar(data):
-	return min(data['features'], key=get_seat_count)
+def get_smallest_bar(bars_json):
+	get_seat_count = lambda bar: bar['properties']['Attributes']['SeatsCount']
+	return min(bars_json['features'], key=get_seat_count)
 
 
-def get_closest_bar(data, longitude, latitude):
-  	def distance(data):
-  		return ((data["geometry"]["coordinates"][0] - longitude)**2 + (data["geometry"]["coordinates"][1] - latitude)**2)**0.5
-
-  	return min(data['features'], key = distance)
+def get_closest_bar(bars_json, longitude, latitude):
+	def distance(bar):
+		return ((bar["geometry"]["coordinates"][0] - longitude)**2 +
+				(bar["geometry"]["coordinates"][1] - latitude)**2)**0.5
+	return min(bars_json['features'], key = distance)
 
 
 if __name__ == '__main__':
