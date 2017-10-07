1396acfcf83e3c2904768930bd623536af50fb54
oakoak <oakduboak.gmail.com>
2017-10-07 14:14:53 +0300

    написан функционал

--- bars.py
+++ bars.py
@@ -1,21 +1,30 @@
 import json
 
 
+def get_seat_count(data):
+	return data['properties']['Attributes']['SeatsCount']
+
+
 def load_data(filepath):
-    pass
+	f = open(filepath, 'r')
+	return json.loads(f.read())
 
 
+		
 def get_biggest_bar(data):
-    pass
+	return max(data['features'], key=get_seat_count)
 
 
 def get_smallest_bar(data):
-    pass
+	return min(data['features'], key=get_seat_count)
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+  	def distance(data):
+  		return ((data["geometry"]["coordinates"][0] - longitude)**2 + (data["geometry"]["coordinates"][1] - latitude)**2)**0.5
+
+  	return min(data['features'], key = distance)
 
 
 if __name__ == '__main__':
-    pass
+	pass
