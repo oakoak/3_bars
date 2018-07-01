f2c39c6fec39fec30235ece8c0ecfb0d19e3103b
varan <varan@VARAN-PC>
2018-07-01 11:58:39 +0300

    d

--- bars.py
+++ bars.py
@@ -7,20 +7,20 @@ def load_data(filepath):
 
         
 def get_biggest_bar(bars_json):
-    return max(bars_json['features'], 
-        key = lambda bar: bar['properties']['Attributes']['SeatsCount'])
+    return max(bars_json['features'],
+               key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_smallest_bar(bars_json):
-    return min(bars_json['features'], 
-        key = lambda bar: bar['properties']['Attributes']['SeatsCount'])
+    return min(bars_json['features'],
+               key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_closest_bar(bars_json, longitude, latitude):
     def distance(bar):
-        return (bar["geometry"]["coordinates"][0] - longitude)**2 +
-                (bar["geometry"]["coordinates"][1] - latitude)**2
-    return min(bars_json['features'], key = distance)
+        return (bar["geometry"]["coordinates"][0] - longitude)**2 + \
+               (bar["geometry"]["coordinates"][1] - latitude)**2
+    return min(bars_json['features'], key=distance)
 
 
 if __name__ == '__main__':
