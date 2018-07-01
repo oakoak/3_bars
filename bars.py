6b065a449be27ac637865a7f4226c8f6bf731ded
varan <varan@VARAN-PC>
2018-07-01 21:35:42 +0300

    fix name of variable

--- bars.py
+++ bars.py
@@ -10,28 +10,28 @@ def get_seats(bar):
     return bar['properties']['Attributes']['SeatsCount']
 
 
-def get_biggest_bar(bars_json):
-    return max(bars_json['features'], key=get_seats)
+def get_biggest_bar(bars):
+    return max(bars['features'], key=get_seats)
 
 
-def get_smallest_bar(bars_json):
-    return min(bars_json['features'], key=get_seats)
+def get_smallest_bar(bars):
+    return min(bars['features'], key=get_seats)
 
 
-def get_closest_bar(bars_json, longitude, latitude):
-    return min(bars_json['features'], key=lambda bar: (bar["geometry"]["coordinates"][0] - longitude) ** 2 +
+def get_closest_bar(bars, longitude, latitude):
+    return min(bars['features'], key=lambda bar: (bar["geometry"]["coordinates"][0] - longitude) ** 2 +
                                                       (bar["geometry"]["coordinates"][1] - latitude) ** 2)
 
 
 if __name__ == '__main__':
     x_gps = float(input("longitude:"))
     y_gps = float(input("latitude:"))
-    data = load_data("bars.json")
-    if data is None :
+    bars = load_data("bars.json")
+    if bars is None:
         exit("data is invalid")
-    max_bar = get_biggest_bar(data)
-    min_bar = get_smallest_bar(data)
-    near_bar = get_closest_bar(data, x_gps, y_gps)
+    max_bar = get_biggest_bar(bars)
+    min_bar = get_smallest_bar(bars)
+    near_bar = get_closest_bar(bars, x_gps, y_gps)
     print("\nThe biggest moscow's bar is:")
     print(max_bar)
     print("\nThe smallest moscow's bar is ")
