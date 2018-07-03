6fd1e6b27c58877c32c2345c0bf02b6b7f6c4702
varan <varan@VARAN-PC>
2018-07-03 23:42:30 +0300

    fix format

--- bars.py
+++ bars.py
@@ -1,40 +1,59 @@
 import json
-
+import sys
 
 def load_data(filepath):
-    with open(filepath, 'r', encoding='utf-8') as file_json:
+    with open(filepath, "r", encoding="utf-8") as file_json:
         return json.loads(file_json.read())
 
 
 def get_seats(bar):
-    return bar['properties']['Attributes']['SeatsCount']
+    return bar["properties"]["Attributes"]["SeatsCount"]
 
 
 def get_biggest_bar(bars):
-    return max(bars['features'], key=get_seats)
+    return max(bars, key=get_seats)
 
 
 def get_smallest_bar(bars):
-    return min(bars['features'], key=get_seats)
+    return min(bars, key=get_seats)
 
 
 def get_closest_bar(bars, longitude, latitude):
-    return min(bars['features'], key=lambda bar: (bar["geometry"]["coordinates"][0] - longitude) ** 2 +
-                                                      (bar["geometry"]["coordinates"][1] - latitude) ** 2)
+    return min(bars, key=lambda bar:
+                        (bar["geometry"]["coordinates"][0] - longitude) ** 2 +
+                        (bar["geometry"]["coordinates"][1] - latitude) ** 2)
 
 
-if __name__ == '__main__':
-    x_gps = float(input("longitude:"))
-    y_gps = float(input("latitude:"))
-    bars = load_data("bars.json")
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(" Usage:python3 bars.py file path to json")
+        exit()
+    x_gps = input("longitude:")
+    y_gps = input("latitude:")
+    while not(is_number(x_gps) and is_number(y_gps)):
+        print("You entered incorrect values, enter again")
+        x_gps = input("longitude:")
+        y_gps = input("latitude:")
+    x_gps = float(x_gps)
+    y_gps = float(y_gps)
+    bars = load_data(sys.argv[1])
     if bars is None:
         exit("data is invalid")
+    bars = bars["features"]
     max_bar = get_biggest_bar(bars)
     min_bar = get_smallest_bar(bars)
     near_bar = get_closest_bar(bars, x_gps, y_gps)
     print("\nThe biggest moscow's bar is:")
-    print(max_bar)
+    print(max_bar["properties"]["Attributes"]["Name"])
     print("\nThe smallest moscow's bar is ")
-    print(min_bar)
+    print(min_bar["properties"]["Attributes"]["Name"])
     print("\nThe nearest moscow's bar is ")
-    print(near_bar)
+    print(near_bar["properties"]["Attributes"]["Name"])
