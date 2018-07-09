3058783e84089815b92fc52223ed24406cc8ef6b
varan <varan@VARAN-PC>
2018-07-09 10:51:16 +0300

    fix entry in try/except

--- bars.py
+++ bars.py
@@ -47,20 +47,20 @@ def main():
     try:
         file_path = sys.argv[1]
         bars = load_data(file_path)
-        gps_coordinates = get_user_gps()
-
-        if gps_coordinates:
-            print_bar(get_biggest_bar(bars), "Biggest")
-            print_bar(get_smallest_bar(bars), "Smallest")
-            print_bar(get_closest_bar(bars, gps_coordinates[0], gps_coordinates[1]), "Nearest")
-        else:
-            exit("Error: GPS coordinates must be input and float type!")
     except IndexError:
         exit("Error: No filename for reading!")
     except FileNotFoundError:
-        exit("Error: file or path '{0}' not found!\n".format(sys.argv[1]))
+        exit("Error: file or path '{0}' not found!\n".format(file_path))
     except json.JSONDecodeError:
         exit("Error: this is not json-file!")
+
+    gps_coordinates = get_user_gps()
+    if gps_coordinates:
+        print_bar(get_biggest_bar(bars), "Biggest")
+        print_bar(get_smallest_bar(bars), "Smallest")
+        print_bar(get_closest_bar(bars, gps_coordinates[0], gps_coordinates[1]), "Nearest")
+    else:
+        exit("Error: GPS coordinates must be input and float type!")
 
 
 if __name__ == "__main__":
